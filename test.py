0143ca91697985100c88d9cbb2fe8ffb3f60b330
fhamilton <philip.hamilton.ham@gmail.com>
2024-01-03 21:58:34 +0200

    1

--- test.py
+++ test.py
@@ -8,20 +8,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 options = webdriver.ChromeOptions()
 options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
-# driver.implicitly_wait(5)
 
-driver.get('https://rozetka.com.ua/ua/')
 
-searchString = driver.find_element(By.XPATH, '//input[@name="search"]').send_keys('Металошукач Minelab Excalibur II')
-time.sleep(2)
-findButton = driver.find_element(By.XPATH, '//button[@class="button button_color_green button_size_medium search-form__submit"]').click()
-wait = WebDriverWait(driver, 10)
-wait.until(expected_conditions.presence_of_element_located((By.XPATH, '//input[@type="checkbox"]')))
-
-confirmButtonText = driver.find_element(By.XPATH, '//span[@class="ctp-label"]').text
-print(confirmButtonText)
-
-confirmButton = driver.find_element(By.XPATH, '//input[@type="checkbox"]').click()
+driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
+searchLine = driver.find_element(By.XPATH, '//input[@type="search"]').send_keys('Walnuts - 1/4 Kg')
+driver.find_element(By.XPATH, '//div[@class="product-action"]/button').click()
+driver.find_element(By.XPATH, '//a/img[@alt="Cart"]').click()
 
 
 
