9da823ee64aa955d14b93d9352210caba55f56be
fhamilton <philip.hamilton.ham@gmail.com>
2024-01-03 22:09:17 +0200

    add time.sleep

--- test.py
+++ test.py
@@ -12,10 +12,9 @@ driver = webdriver.Chrome(options=options)
 
 driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
 searchLine = driver.find_element(By.XPATH, '//input[@type="search"]').send_keys('Walnuts - 1/4 Kg')
+time.sleep(3)
 driver.find_element(By.XPATH, '//div[@class="product-action"]/button').click()
 driver.find_element(By.XPATH, '//a/img[@alt="Cart"]').click()
-
-
 
 
 
