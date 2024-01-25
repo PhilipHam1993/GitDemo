a531a64a9e9b9fdd2e47da60ae49a1ec1741e942
fhamilton <philip.hamilton.ham@gmail.com>
2024-01-25 13:20:59 +0200

    add crossbrowser fiature

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,25 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
+
 @pytest.fixture(scope="class")
 def browser(request):
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('detach', True)
-    driver = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('detach', True)
+        driver = webdriver.Chrome(options=options)
+
+    elif browser_name == "firefox":
+        driver = webdriver.Firefox()
+    elif browser_name == "EI":
+        driver = webdriver.Edge()
+
     driver.get('https://rahulshettyacademy.com/seleniumPractise/#/')
     driver.maximize_window()
     request.cls.driver = driver
